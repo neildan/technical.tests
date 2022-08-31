f3157347504f022ea6af9d1012f867f0e0788caf
neildan <felipeluiz152000@gmail.com>
2022-08-30 20:19:13 -0500

    First Reverse Coderbyte

---
+++ Coderbyte/firstReverse.js
@@ -0,0 +1,18 @@
+/*
+First Reverse
+Have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order.
+
+Examples:
+1. Input: "coderbyte"
+Output: etybredoc
+2. Input: "I Love Code"
+Output: edoC evoL I
+*/
+
+function FirstReverse(str) {
+  newArrayStr = [];
+  for (i = 0; i <= str.length - 1; i++)
+    newArrayStr[i] = str.split("")[str.length - 1 - i];
+  return newArrayStr.join("");
+}
+console.log(FirstReverse(readline()));
